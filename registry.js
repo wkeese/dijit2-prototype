d8db7c00f2a736264f88622b08bd25e005fc7596
Bill Keese <bill@dojotoolkit.org>
2013-01-22 11:04:26 +0900

    remove dijit/_base except for dijit/_base/manager::defaultDuration def, refs #1

--- registry.js
+++ registry.js
@@ -1,9 +1,7 @@
 define([
 	"dojo/_base/array", // array.forEach array.map
-	"dojo/sniff", // has("ie")
-	"dojo/_base/window", // win.body
-	"./main"	// dijit._scopeName
-], function(array, has, win, dijit){
+	"dojo/_base/window" // win.body
+], function(array, win){
 
 	// module:
 	//		dijit/registry
@@ -78,7 +76,7 @@ define([
 					(widgetType in _widgetTypeCtr ?
 						++_widgetTypeCtr[widgetType] : _widgetTypeCtr[widgetType] = 0);
 			}while(hash[id]);
-			return dijit._scopeName == "dijit" ? id : dijit._scopeName + "_" + id; // String
+			return id; // String
 		},
 
 		findWidgets: function(root, skipNode){
