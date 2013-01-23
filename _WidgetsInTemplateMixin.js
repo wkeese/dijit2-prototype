13c4b504fa98123b7c26ba49eaf980af7b48d3dc
Bill Keese <bill@dojotoolkit.org>
2013-01-23 14:39:22 +0900

    _WidgetsInTemplateMixin: remove support for _earlyTemplatedStartup etc., refs #4

--- _WidgetsInTemplateMixin.js
+++ _WidgetsInTemplateMixin.js
@@ -13,20 +13,6 @@ define([
 		// summary:
 		//		Mixin to supplement _TemplatedMixin when template contains widgets
 
-		// _earlyTemplatedStartup: Boolean
-		//		A fallback to preserve the 1.0 - 1.3 behavior of children in
-		//		templates having their startup called before the parent widget
-		//		fires postCreate. Defaults to 'false', causing child widgets to
-		//		have their .startup() called immediately before a parent widget
-		//		.startup(), but always after the parent .postCreate(). Set to
-		//		'true' to re-enable to previous, arguably broken, behavior.
-		_earlyTemplatedStartup: false,
-
-		// widgetsInTemplate: [protected] Boolean
-		//		Should we parse the template to find widgets that might be
-		//		declared in markup inside it?  (Remove for 2.0 and assume true)
-		widgetsInTemplate: true,
-
 		// contextRequire: Function
 		//		Used to provide a context require to the dojo/parser in order to be
 		//		able to use relative MIDs (e.g. `./Widget`) in the widget's template.
@@ -47,9 +33,8 @@ define([
 					template: true,
 					inherited: {dir: this.dir, lang: this.lang, textDir: this.textDir},
 					propsThis: this,	// so data-dojo-props of widgets in the template can reference "this" to refer to me
-					contextRequire: this.contextRequire,
-					scope: "dojo"	// even in multi-version mode templates use dojoType/data-dojo-type
-				}).then(lang.hitch(this, function(widgets){
+					contextRequire: this.contextRequire
+=				}).then(lang.hitch(this, function(widgets){
 					this._startupWidgets = widgets;
 
 					// _WidgetBase::destroy() will destroy any supporting widgets under this.domNode.
@@ -90,7 +75,7 @@ define([
 			// Override _AttachMixin._processNode to skip DOMNodes with data-dojo-type set.   They are handled separately
 			// in the _beforeFillContent() code above.
 
-			if(getAttrFunc(baseNode, "dojoType") || getAttrFunc(baseNode, "data-dojo-type")){
+			if(getAttrFunc(baseNode, "data-dojo-type")){
 				return true;
 			}
 
